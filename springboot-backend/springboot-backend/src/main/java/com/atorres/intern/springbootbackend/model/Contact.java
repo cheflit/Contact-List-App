1f071bf788b9b8ec66bd791a352fd3bbb278595f
Adrian Torres <58715696+cheflit@users.noreply.github.com>
2022-06-21 02:10:34 -0700

    Initial commit

---
+++ springboot-backend/springboot-backend/src/main/java/com/atorres/intern/springbootbackend/model/Contact.java
@@ -0,0 +1,87 @@
+package com.atorres.intern.springbootbackend.model;
+
+import javax.persistence.*;
+
+// The contact JPA entity
+
+@Entity
+@Table(name = "contacts")
+public class Contact {
+    @Id
+    @GeneratedValue//(strategy = GenerationType.AUTO)
+    private Long id; // primary key (@Id)
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    @Column(name = "email")
+    private String email;
+
+    public Contact() {
+    }
+
+    public Contact(String firstName, String lastName, String phoneNumber, String email) {
+        super();
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Contact{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
